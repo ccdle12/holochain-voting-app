1bd127a5ae58f4cbb921dd79517205ca8cfaf8bd
ccdle12 <chris.coverdale24@gmail.com>
2017-12-10 14:14:47 +0000

    update

--- ui/hc.js
+++ ui/hc.js
@@ -1,1 +1,33 @@
-function yourApp(){alert('your UI code here!')}
+// Copyright (C) 2013-2017, The MetaCurrency Project (Eric Harris-Braun & Arthur Brock)
+// Use of this source code is governed by GPLv3 found in the LICENSE file
+//---------------------------------------------------------------------------------------
+
+// Holochain UI library
+
+// use send to make an ajax call to your exposed functions
+function send(fn,data,resultFn) {
+    if (VotingApp.debug) console.log("calling: " + fn+" with "+JSON.stringify(data));
+    
+    eval(VotingApp.getHook(fn, "preSendHook"));
+    $.post(
+        "/fn/VotingApp/"+fn,
+        data,
+        function(response) {
+            if (VotingApp.debug) console.log("response: " + response);
+            eval(VotingApp.getHook(fn, "successPreResult"));
+            resultFn(response);
+            eval(VotingApp.getHook(fn, "successPostResult"));
+        }
+    ).error(function(response) {
+        console.log("response failed: " + response.responseText);
+        $(".error").html("<span>" + response.responseText + "<span>").toggleClass("show", true)
+        setTimeout
+        ( () =>
+          { $(".error")  .toggleClass("show", false);
+            eval(VotingApp.getHook(fn, "errorTimeoutComplete"));
+          },
+          2000
+        );
+    })
+    ;
+};
---
+++ ui/votingapp.js
@@ -0,0 +1,40 @@
+console.log("I AM A CONSOLE LOG");
+VotingApp = 
+{ "debug": false,
+
+  "functionSpecDict":
+      { "appProperty":{"preSendHook":"VotingApp.noOp=true;","successPreResult":"VotingApp.noOp=true;","successPostResult":"VotingApp.noOp=true;"},
+        "getPostsBy":{"preSendHook":"VotingApp.noOp=true;","successPreResult":"VotingApp.noOp=true;","successPostResult":"VotingApp.noOp=true;"},
+        "newHandle":{"preSendHook":"$(\".spinner\").toggleClass(\"show\", true);","successPreResult":"VotingApp.noOp=true;","successPostResult":"$(\".spinner\").toggleClass(\"show\", false);"},
+        "getAgent":{"preSendHook":"$(\".spinner\").toggleClass(\"show\", true);","successPreResult":"VotingApp.noOp=true;","successPostResult":"VotingApp.noOp=true;"},
+        "follow":{"preSendHook":"$(\".spinner\").toggleClass(\"show\", true);","successPreResult":"VotingApp.noOp=true;","successPostResult":"$(\".spinner\").toggleClass(\"show\", false);"},
+        "post":{"preSendHook":"$(\".logo > img\").toggleClass(\"spin\", true);","successPreResult":"VotingApp.noOp=true;","successPostResult":"$(\".logo > img\").toggleClass(\"spin\", false);"},
+        "manual":{"clearUI":"$(\".spinner\").toggleClass(\"show\", false);","normalUI":"$(\".spinner\").toggleClass(\"show\", false);"},
+        "getHandle":{"preSendHook":"$(\"#changeHandleButton\").toggleClass(\"colorCycle\", true);","successPreResult":"VotingApp.noOp=true;","successPostResult":"$(\"#changeHandleButton\").toggleClass(\"colorCycle\", false);"},
+        "getFollow":{"preSendHook":"$('#followButton').toggleClass('colorCycle', true);","successPreResult":"VotingApp.noOp=true;","successPostResult":"$('#followButton').toggleClass('colorCycle', false);"},
+
+      },
+  "getHook": 
+      (functionName, hookName) =>
+      { if (!VotingApp.functionSpecDict.hasOwnProperty(functionName))
+        { VotingApp.functionSpecDict[functionName]  = {};
+        }
+        if (!VotingApp.functionSpecDict[functionName].hasOwnProperty(hookName))
+        { VotingApp.functionSpecDict[functionName][hookName] = "";
+        }
+        return VotingApp.functionSpecDict[functionName][hookName];
+      },
+};
+
+function getMyHandle()
+{
+    send("getMyHandle", undefined, function(handle)
+    {
+        console.log(handle);
+    })
+}
+
+$(window).ready(function() 
+{
+    getMyHandle();
+})
